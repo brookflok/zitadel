7d0bd84275e14fc3612c2dd84437bbfb47014a6f
Damir Arnautovic <damir.arnautovic@bigburg.de>
2026-05-22 14:16:10 +0200

    feat(login): force locale to Croatian (hr) only

--- apps/login/src/lib/i18n.ts
+++ apps/login/src/lib/i18n.ts
@@ -5,56 +5,8 @@ export interface Lang {
 
 export const LANGS: Lang[] = [
   {
-    name: "English",
-    code: "en",
-  },
-  {
-    name: "Deutsch",
-    code: "de",
-  },
-  {
-    name: "Italiano",
-    code: "it",
-  },
-  {
-    name: "Español",
-    code: "es",
-  },
-  {
-    name: "Français",
-    code: "fr",
-  },
-  {
-    name: "Nederlands",
-    code: "nl",
-  },
-  {
-    name: "Polski",
-    code: "pl",
-  },
-  {
-    name: "简体中文",
-    code: "zh",
-  },
-  {
-    name: "Русский",
-    code: "ru",
-  },
-  {
-    name: "Türkçe",
-    code: "tr",
-  },
-  {
-    name: "日本語",
-    code: "ja",
-  },
-  {
-    name: "Українська",
-    code: "uk",
-  },
-  {
-    name: "العربية",
-    code: "ar",
+    name: "Hrvatski",
+    code: "hr",
   },
 ];
 
